e48b5c0d3d75943b01fcc95440a917986d3f13c9
Alison Medeiro Silva <60613170+alison-medeiro@users.noreply.github.com>
2023-11-22 20:15:04 -0300

    Create python.py

---
+++ python.py
@@ -0,0 +1,29 @@
+
+# Online Python - IDE, Editor, Compiler, Interpreter
+
+import sqlite3
+
+DATABASE = 'user.db'
+
+conn = sqlite3.connect(DATABASE)
+
+cursor = conn.cursor()
+
+cursor.execute('''
+    CREATE TABLE IF NOT EXISTS Usuarios(
+        id INTEGER PRIMARY KEY,
+        nome TEXT,
+        idade INTEGER
+    )
+    ''')
+
+cursor.execute('INSERT INTO Usuarios(nome, idade) VALUES (?,?)', ('John', '19'))
+cursor.execute('INSERT INTO Usuarios(nome, idade) VALUES (?,?)', ('Alison', '23'))
+
+conn.commit()
+
+cursor.execute('SELECT * FROM Usuarios')
+for row in cursor.fetchall():
+    print(row)
+    
+conn.close()    
